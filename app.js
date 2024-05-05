1fbce02c9b36c12dfbf8eea9ada6462b45aba90f
ridowan <ridowansikder554@gmail.com>
2024-05-05 14:19:54 +0600

    broadcasting implimented

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 const express = require('express')
 const app = express()
+
+//socket.io setup
+const http = require('http')
+const server = http.createServer(app)
+const { Server } = require('socket.io')
+const io = new Server(server, { pingInterval: 2000, pingTimeout: 5000 })
 const port = 3000
 
 app.use(express.static('public'))
@@ -8,6 +14,25 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html')
 })
 
-app.listen(port, () => {
+//this will keep all the player states
+const players = {}
+
+//fires everytime a user conncets 
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  players[socket.id] = {
+    x: 500 * Math.random(),
+    y: 500 * Math.random()
+  }
+  // broadcast the data to ALL the connceted clients 
+  io.emit('updatePlayer', players)
+
+  socket.on('disconnect', (reason) => {
+    delete players[socket.id]
+    io.emit('updatePlayer', players)
+  })
+});
+
+server.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
